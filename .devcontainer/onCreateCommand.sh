1ab3c9a58fd10dc8ebb40e41641f69fdaae091e5
Mike Wallio <michael.wallio@outlook.com>
2025-10-11 13:00:23 -0400

    Add devcontainer

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Setup bun
+curl -fsSL https://bun.sh/install | sudo BUN_INSTALL=/usr/local bash
---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+cd /workspaces/userscripts
+bun install
